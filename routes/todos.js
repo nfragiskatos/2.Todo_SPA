e65d93d08d2ec8b641a1d82ef8c602bb2a089f16
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-10-10 00:14:06 +0000

    Added DELETE route

--- routes/todos.js
+++ routes/todos.js
@@ -49,4 +49,16 @@ router.put("/:todoId", function(req, res) {
     });
 });
 
+// DELETE
+router.delete("/:todoId", function(req, res) {
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function() {
+        res.json({message: "We deleted it!"});
+        
+    })
+    .catch(function(err) {
+        res.send(err);
+    });
+});
+
 module.exports = router;
