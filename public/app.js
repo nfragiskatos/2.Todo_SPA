0cebbb6ba99a7f9c0ccf97d4c92494e81f0ea03c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-10-10 23:35:52 +0000

    Added ability to create a new one by using the input and creating a new DB entry by sending a request to the API

--- public/app.js
+++ public/app.js
@@ -1,11 +1,36 @@
 $(document).ready(function() {
     $.getJSON("/api/todos")
     .then(addTodos);
+    
+    $("#todoInput").keypress(function(event) {
+        if (event.which == 13) {
+            createTodo();
+        } 
+    });
 });
 
 function addTodos(todos) {
     todos.forEach(function(todo) {
-        var newTodo = $("<li class=\"task\">" + todo.name + "</li>");
-        $(".list").append(newTodo);
+        addTodo(todo);
+    });
+}
+
+function addTodo(todo) {
+    var newTodo = $("<li class=\"task\">" + todo.name + "</li>");
+    if (todo.completed) {
+        newTodo.addClass("done");
+    }
+    $(".list").append(newTodo);
+}
+
+function createTodo() {
+    var userInput = $("#todoInput").val();
+    $.post("/api/todos", {name: userInput})
+    .then(function(newTodo) {
+        $("#todoInput").val("");
+        addTodo(newTodo);
+    })
+    .catch(function(err) {
+        console.log(err); 
     });
 }
